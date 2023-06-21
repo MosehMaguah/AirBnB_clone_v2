f861ea74bc0f1c5d7a0e5300394df719f585ee33
MosehMaguah <Mosehmaguah@gmail.com>
2023-06-21 18:10:11 +0300

    web static files

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+  this srcipt starts a Flask web application
 """
 
 from flask import Flask
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+  This script starts a Flask web application
 """
 
 from flask import Flask, render_template
@@ -21,20 +21,22 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """ display “C ” followed by the value
+        of the text variable """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+    """ display “Python ”, followed by the
+        value of the text variable """
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def imanumber(n):
-    """display “n is a number” only if n is an integer"""
+    """ display “n is a number” only if n is an integer """
     return "{:d} is a number".format(n)
 
 
