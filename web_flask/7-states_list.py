ce7ade4ec3ff7d90ad7ea686de31b6837ea54c08
MosehMaguah <Mosehmaguah@gmail.com>
2023-07-24 01:43:22 +0300

    Airbnb clone flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,27 +5,21 @@ The script that starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """displays a HTML page with all states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def handle_teardow(self):
-    """
-    After each request you have to kill the current session of SQLAlchemy
-    """
+def teardown_db(exception):
+    """close the database on teardown"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_of_state():
-    """
-    Function called with the route /states_list
-    """
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
-
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
